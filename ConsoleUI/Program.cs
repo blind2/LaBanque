8ce0859542ed616baa426cc7e4bb47aabab8c22f
Blind2 <trudelle.alexandre@carrefour.cegepvicto.ca>
2020-04-29 20:51:35 -0400

    Modification du main

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,4 @@
 ﻿using BanqueLibrairie;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
@@ -12,24 +7,27 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //Créations des objets
-            Banque banque1 = new Banque("Desjardins","815");
+            Banque banque1 = new Banque("815");
             Succursale succursale1 = new Succursale("Caisse Desjardin", "300, boulevard des Bois-Francs Sud");
-            Client client1 = new Client("Alexandre", "Trudelle", "52 rue fournier", "819-740-7201", "G6S2K9");
+            Client client1 = new Client("Alexandre", "Trudelle", "123 rue", "819-123-123", "G6S2K9");
             Compte compte1 = new Compte("Cheque");
-            Compte compte2 = new Compte("Cheque");
-
-            //Met les obejets les un dans les autres
+            Compte compte2 = new Compte("epargne");
+           
             banque1.AjouterSuccursale(succursale1);
             succursale1.AjouterUnClient(client1);
-            client1.RechercherUnCompte("815-001-001-001");
+            client1.AjouterUnCompte(compte1);
+            compte1.Deposer(500);
             
-            banque1.DeposerDans("815-001-001");
-            succursale1.RechercherUnCompte("815-001-001");
+            //Moyen de Recherche
+            client1.RechercherUnCompte("815-001-001-001");           
+            succursale1.RechercherUnClient("815-001-001-001");
+            banque1.RechercherUneSuccursale("815-001-001-001");
+            
 
-           
-            compte1.VoirSolde();
-            compte1.CompareTo(compte2);
+
+            compte1.VoireLeSoldeDuCompte("815-001-001-001");
             
+
 
         }
     }
